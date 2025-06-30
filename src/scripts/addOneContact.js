a798725bcecf2eeca10f08a1ad7a850e99d33b05
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-06-30 12:18:14 +0300

    addonecontact js build

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+const readContacts = require('../utils/readContacts');
+const writeContacts = require('../utils/writeContacts');
+const createFakeContact = require('../utils/createFakeContact');
+
+const addOneContact = async () => {
+  const contacts = await readContacts();
+  contacts.push(createFakeContact());
+  await writeContacts(contacts);
+};
 
 addOneContact();
