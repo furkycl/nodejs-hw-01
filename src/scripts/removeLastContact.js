db0cff4850cb1fb39ecbc3c7b23d350bca222b3b
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-06-30 12:23:23 +0300

    remove last person if exist otherwise console.log error removelastcontact.js build

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,12 @@
-export const removeLastContact = async () => {};
+const readContacts = require('../utils/readContacts');
+const writeContacts = require('../utils/writeContacts');
+
+const removeLastContact = async () => {
+  const contacts = await readContacts();
+  if (contacts.length === 0) return console.log('Silinecek kişi yok.');
+  contacts.pop();
+  await writeContacts(contacts);
+  console.log('Son kişi silindi.');
+};
 
 removeLastContact();
