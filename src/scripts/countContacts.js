ab5936deb379c512ae0dfd97d730c707cf494783
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-06-30 12:20:55 +0300

    to see how many ppl in contact countcontact.js build

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,8 @@
-export const countContacts = async () => {};
+const readContacts = require('../utils/readContacts');
 
-console.log(await countContacts());
+const countContacts = async () => {
+  const contacts = await readContacts();
+  console.log(`Toplam kişi sayısı: ${contacts.length}`);
+};
+
+countContacts();
