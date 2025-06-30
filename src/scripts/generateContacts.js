06707f66f691a2fc5dd7ff72e7e5e4d9d01abb29
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-06-30 12:17:29 +0300

    generate random person data generatecontact.js build

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+const readContacts = require('../utils/readContacts');
+const writeContacts = require('../utils/writeContacts');
+const createFakeContact = require('../utils/createFakeContact');
 
-generateContacts(5);
+const generateContacts = async (number = 5) => {
+  const contacts = await readContacts();
+  const newContacts = Array.from({ length: number }, () => createFakeContact());
+  const updated = [...contacts, ...newContacts];
+  await writeContacts(updated);
+};
+
+generateContacts();
